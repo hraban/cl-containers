b8e399ba08141c65ab28bd8233cc77927fe703cf
Gary King <gwking@metabang.com>
2007-10-24 10:20:55 -0400

    Added priority-queue.test (via Josip Gracin for starting it)
    darcs-hash:20071024142055-3cc5d-023bc9a82750c15351fbc2be19c04216cda3afe9.gz

---
+++ test/priority-queues.lisp
@@ -0,0 +1,32 @@
+(in-package #:cl-containers-test)
+
+(deftestsuite test-priority-queue (cl-containers-test)
+  ((q (make-instance 'priority-queue-on-container
+                     :sorter #'string<
+                     :test #'string=))))
+
+(deftestsuite test-delete-priority-queue (test-priority-queue)
+  ()
+  (:equality-test 'string=)
+  (:setup (insert-item q "a")
+          (insert-item q "b")
+          (insert-item q "c")))
+
+(addtest (test-delete-priority-queue)
+  delete-first-1
+  (ensure-same (first-element q) "a")
+  (delete-first q)
+  (ensure-same (first-element q) "b")
+  (delete-first q)
+  (ensure-same (first-element q) "c"))
+
+(addtest (test-delete-priority-queue)
+  delete-first-2
+  (dequeue q)
+  (ensure-same (first-element q) "b")
+  (dequeue q)
+  (ensure-same (first-element q) "c")
+  (dequeue q)
+  (ensure (empty-p q)))
+
+
