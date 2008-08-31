63701d54ee7f65dd7d336e5bd4f0339170351d33
Gary King <gwking@metabang.com>
2008-08-31 16:19:34 -0400

    cleanup comments
    darcs-hash:20080831201934-3cc5d-512b8ee61ec36f1fc927fad610a224b6affeb441.gz

--- dev/queues.lisp
+++ dev/queues.lisp
@@ -65,8 +65,6 @@
         (apply #'make-container container-type args))
   (call-next-method))
 
-;;; ---------------------------------------------------------------------------
-
 (defmethod insert-item ((q priority-queue-on-container) item)
   (insert-item (container q) item))
 
@@ -95,40 +93,26 @@
   (let ((node (find-item (container q) item)))
     (when node (element node))))
 
-;;; ---------------------------------------------------------------------------
-
 (defmethod find-node ((q priority-queue-on-container) (item t))
   (find-node (container q) item))
-
-;;; ---------------------------------------------------------------------------
 
 (defmethod find-element ((q priority-queue-on-container) (item t))
   (find-element (container q) item))
 
-;;; ---------------------------------------------------------------------------
-
 (defmethod delete-item ((q priority-queue-on-container) (item t))
   (delete-item (container q) item))
-
-;;; ---------------------------------------------------------------------------
 
 (defmethod delete-node ((q priority-queue-on-container) (item t))
   (delete-node (container q) item))
 
-;;; ---------------------------------------------------------------------------
-
 (defmethod delete-element ((q priority-queue-on-container) (item t))
   (delete-element (container q) item))
-
-;;; ---------------------------------------------------------------------------
 
 (defmethod delete-item-if (test (q priority-queue-on-container))
   (delete-item-if test (container q)))
 
 (defmethod iterate-nodes ((q priority-queue-on-container) fn)
   (iterate-nodes (container q) fn))
-
-;;; ---------------------------------------------------------------------------
 
 (defmethod iterate-elements ((q priority-queue-on-container) fn)
   (iterate-elements (container q) fn))
@@ -143,8 +127,6 @@
   ((queue nil :accessor queue-queue)
    (indexer nil :accessor queue-header))
   (:documentation "A simple FIFO queue implemented as a list with extra bookkeeping for efficiency."))
-
-;;; ---------------------------------------------------------------------------
 
 ;; Some semantically helpful functions
 (defun front-of-queue (queue)
@@ -162,8 +144,6 @@
   (proclaim '(inline front-of-queue front-of-queue!))
   (proclaim '(inline tail-of-queue tail-of-queue!)))
 
-;;; ---------------------------------------------------------------------------
-
 (defmethod insert-item ((q basic-queue) (item t))
   "Add an item to the queue."
   (let ((new-item (list item)))
@@ -174,8 +154,6 @@
            (setf (cdr (tail-of-queue q)) new-item
                  (tail-of-queue q) new-item))))
   q)
-
-;;; ---------------------------------------------------------------------------
 
 (defmethod delete-first ((q basic-queue))
   (let ((result (front-of-queue q)))
@@ -188,19 +166,13 @@
     
     result))
 
-;;; ---------------------------------------------------------------------------
-
 (defmethod empty-p ((q basic-queue))
   (null (queue-header q)))
-
-;;; ---------------------------------------------------------------------------
 
 (defmethod iterate-nodes ((q basic-queue) fn)
   (let ((start (front-of-queue q)))
     (mapc fn start))
   (values q))
-
-;;; ---------------------------------------------------------------------------
 
 (defmethod size ((q basic-queue))
   ;;??slow
@@ -208,27 +180,19 @@
     0
     (length (front-of-queue q))))
 
-;;; ---------------------------------------------------------------------------
-
 (defmethod first-element ((q basic-queue))
   "Returns the first item in a queue without changing the queue."
   (car (front-of-queue q)))
 
-;;; ---------------------------------------------------------------------------
-
 (defmethod (setf first-element) (value (q basic-queue))
   "Returns the first item in a queue without changing the queue."
   (setf (car (front-of-queue q)) value))
-
-;;; ---------------------------------------------------------------------------
 
 (defmethod empty! ((q basic-queue))
   "Empty a queue of all contents."
   (setf (queue-queue q) nil
         (queue-header q) nil)
   (values))
-
-;;; ---------------------------------------------------------------------------
 
 (defmethod delete-item ((queue basic-queue) item)
   (unless (empty-p queue)
